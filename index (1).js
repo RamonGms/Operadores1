1462278ea0eaa25447a9a3cea7205210a0240a80
RamonGms <106332513+RamonGms@users.noreply.github.com>
2023-05-26 22:49:46 -0300

    Add files via upload

---
+++ index (1).js
@@ -0,0 +1,18 @@
+//console.log(10+1);
+//console.log(10*(2+1)-3);
+
+let valor = 10;
+
+//Soma
+console.log(valor + 9);
+//Sutração
+console.log(valor - 9);
+//Mutiplicação
+console.log(valor * 9);
+//Divisão
+console.log(valor / 9);
+//Poteciação
+console.log(valor ** 9);
+//resto da divisão
+console.log(valor % 9);
+
